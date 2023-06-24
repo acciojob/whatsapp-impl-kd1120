23e04a6ac90c98670213ca5a5d50565162c2f073
khushboo <khushidudeja45@gmail.com>
2023-06-24 19:29:21 +0530

    1st

--- src/main/java/com/driver/WhatsappRepository.java
+++ src/main/java/com/driver/WhatsappRepository.java
@@ -16,6 +16,14 @@ public class WhatsappRepository {
     private HashSet<String> userMobile;
     private int customGroupCount;
     private int messageId;
+    private int customGroupCount;
+    private int messageId;
+
+    public void GroupRepository() {
+        groupMap = new HashMap<>();
+    }
+
+
 
     public WhatsappRepository(){
         this.groupMessageMap = new HashMap<Group, List<Message>>();
@@ -23,7 +31,67 @@ public class WhatsappRepository {
         this.senderMap = new HashMap<Message, User>();
         this.adminMap = new HashMap<Group, User>();
         this.userMobile = new HashSet<>();
-        this.customGroupCount = 0;
-        this.messageId = 0;
+        this.customGroupCount = 1;
+        this.messageId = 1;
     }
-}
+
+    public String createUser(String name, String mob) throws Exception{
+
+        if(userMobile.contains(mob)){
+           throw new Exception("user already exist");
+        }
+
+        else {
+            userMobile.add(mob);
+        }
+   return "SUCCESS";
+    }
+
+   public Group createGroup(List<User> users){
+        if(users.size()==2){
+
+        }
+   }
+
+   public int createMessage(String content){
+
+        Message message = new Message(content);
+        messageId++;
+        return message.getId();
+   }
+
+    public int sendMessage(Message message, User sender, Group group) throws Exception{
+
+
+    }
+
+    public String changeAdmin(User approver, User user, Group group) throws Exception{
+
+        if(!groupUserMap.containsKey(group)){
+            throw new Exception("Group does not exist");
+        }
+        if(!approver.getMobile().equals(adminMap.get(group).getMobile())){
+            throw new Exception("Approver does not have rights");
+        }
+        List<User> users = new ArrayList<>(groupUserMap.get(group));
+        for(User x : users){
+            if(x.getMobile().equals(user.getMobile())){
+                adminMap.put(group, user);
+                return "SUCCESS";
+            }
+        }
+        throw new Exception("User is not a participant");
+    }
+    public int removeUser(User user) throws Exception{
+
+    }
+
+    public String findMessage(Date start, Date end, int K) throws Exception{
+
+    }
+
+
+
+
+    }
+
